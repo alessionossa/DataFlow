863546e66cb9ec1c7eac76719081fe0f237357f8
Alessio Nossa <alessio.nossa@gmail.com>
2024-02-25 20:24:32 +0100

    Allow changes in StringNode

--- Sources/Flow/Nodes/StringNode.swift
+++ Sources/Flow/Nodes/StringNode.swift
@@ -18,11 +18,35 @@ public class StringNode: BaseNode {
     struct StringMiddleView: View {
         @ObservedObject var node: StringNode
         
+        var valueBinding: Binding<String> {
+            Binding<String>(
+                get: {
+                    return node.valueUpdate?.value ?? ""
+                },
+                set: { newValue in
+                    self.node.valueUpdate = ValueUpdate(newValue)
+                }
+            )
+        }
+        
         var body: some View {
-            Text("\(node.valueUpdate?.value ?? "")")
-                .font(.callout.monospaced())
-                .lineLimit(nil)
-                .frame(width: 150)
+            if #available(iOS 16.0, macOS 13.0, *) {
+                TextField("Text", text: valueBinding, axis: .vertical)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2...6)
+                    .font(.callout.monospaced())
+                    .textFieldStyle(.roundedBorder)
+                    .frame(width: 192)
+            } else {
+                TextEditor(text: valueBinding)
+                    .frame(minHeight: 40, maxHeight: 80)
+                    .multilineTextAlignment(.leading)
+                    .font(.callout.monospaced())
+                    .textFieldStyle(.roundedBorder)
+                    .frame(width: 192)
+                    .fixedSize(horizontal: true, vertical: true)
+            }
+            
         }
     }
 
